abe16ae4e6cf026386c70f6bf5dc08ce8019b4ca
Leonie von Schirach <leonie.schirach@web.de>
2025-11-20 11:53:59 +0000

    add tokeniser method test to check that input type can only be str

--- test_tokeniser.py
+++ test_tokeniser.py
@@ -6,3 +6,10 @@ def test_tokenise_returns_list_of_string_input():
     output = t.tokenise("hello")
     assert output == ["hello"]
 
+# Test 2
+def test_tokeniser_gives_error_when_input_not_str():
+    t = Tokeniser()
+    num = t.tokenise(0)
+    list_input = t.tokenise(['hello'])
+    assert num == 'Input text must be a string'
+    assert list_input == 'Input text must be a string'
