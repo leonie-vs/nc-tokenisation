c15965c48fa413f717a21a106b2d695e8fa60504
Leonie von Schirach <leonie.schirach@web.de>
2025-11-20 12:00:05 +0000

    make test 3 pass by using split() method on input text

--- tokeniser.py
+++ tokeniser.py
@@ -2,6 +2,7 @@ class Tokeniser:
 
     def tokenise(self, text: str) -> list[str]:
         if type(text) == str:
-            return [text]
+            words = text.split()
+            return words
         else:
             return 'Input text must be a string'
