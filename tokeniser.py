18c4f0c39ca5766ac77730e5bea3bab1c244721b
Leonie von Schirach <leonie.schirach@web.de>
2025-11-20 11:54:49 +0000

    make test 2 pass by returning message when input not str

--- tokeniser.py
+++ tokeniser.py
@@ -1,4 +1,7 @@
 class Tokeniser:
 
     def tokenise(self, text: str) -> list[str]:
-        return [text]
+        if type(text) == str:
+            return [text]
+        else:
+            return 'Input text must be a string'
