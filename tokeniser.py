f977cdd9b936460b70f7077c5fed9ac15b9f6ebc
Leonie von Schirach <leonie.schirach@web.de>
2025-11-20 12:11:34 +0000

    change tokeniser method to handle punctuations and pass test 5

--- tokeniser.py
+++ tokeniser.py
@@ -1,8 +1,10 @@
+import re
+
 class Tokeniser:
 
     def tokenise(self, text: str) -> list[str]:
         if type(text) == str:
-            words = [word for word in text.split() if word]
+            words = [word for word in re.split(r'[ .,!?:]+', text) if word]
             return words
         else:
             return 'Input text must be a string'
