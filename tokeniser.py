87c906842fe4820265c6fc757501989f50de7387
Leonie von Schirach <leonie.schirach@web.de>
2025-11-20 17:21:33 +0000

    add merge_most_frequent_pair method to Tokeniser class and pass test 17

--- tokeniser.py
+++ tokeniser.py
@@ -26,21 +26,38 @@ class Tokeniser:
     def split_into_subwords(self, tokens: list[str]) -> list[list[str]]:
         if tokens == []:
             return 'Empty input list'
-        subwords = []
+        subword_tokens = []
         for word in tokens:
             split_word = [ch for ch in word]
             split_word.append(self.END_OF_WORD_SYMBOL)
-            subwords.append(split_word)
-        return subwords
+            subword_tokens.append(split_word)
+        return subword_tokens
     
     def count_symbol_pairs(self, subword_tokens: list[list[str]]) -> dict[tuple[str, str], int]:
         adjacent_pairs = []
         for word in subword_tokens:
             for i in range(len(word) - 1):
                 adjacent_pairs.append((word[i], word[i + 1]))
-        pair_count = Counter(adjacent_pairs)
-        return pair_count
-
-
+        pair_counts = Counter(adjacent_pairs)
+        return pair_counts
+    
+    def merge_most_frequent_pair(self, subword_tokens: list[list[str]], pair_counts: dict[tuple[str, str], int]) -> list[list[str]]:        
+        sorted_pairs = self.sort_vocab(pair_counts)
+        most_frequent_pair = sorted_pairs[0][0]
+        a, b = most_frequent_pair
+        merged_tokens = []
+        for token in subword_tokens:
+            new_token = []
+            i = 0
+            while i < len(token):
+                if i < len(token) - 1 and token[i] == a and token[i+1] == b:
+                    new_token.append(a + b)
+                    i += 2
+                else:
+                    new_token.append(token[i])
+                    i += 1
+            merged_tokens.append(new_token)
+        return merged_tokens
+        
 
         
