b171051585456d9b2793ba944df87e68bc27e5b3
Leonie von Schirach <leonie.schirach@web.de>
2025-11-20 12:21:11 +0000

    refactor tokenise method and add lower() method to make test 6 pass

--- tokeniser.py
+++ tokeniser.py
@@ -3,8 +3,10 @@ import re
 class Tokeniser:
 
     def tokenise(self, text: str) -> list[str]:
-        if type(text) == str:
-            words = [word for word in re.split(r'[ .,!?:]+', text) if word]
-            return words
-        else:
+        if type(text) != str:
             return 'Input text must be a string'
+        else:
+            lower_text = text.lower()
+            words = [word for word in re.split(r'[ .,!?:]+', lower_text) if word]
+            return words
+        
