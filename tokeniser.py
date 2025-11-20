d795af2a841096dd4ce08dbb3de92d72575084b6
Leonie von Schirach <leonie.schirach@web.de>
2025-11-20 12:04:24 +0000

    make test 4 pass by handling empty spaces in tokeniser

--- tokeniser.py
+++ tokeniser.py
@@ -2,7 +2,7 @@ class Tokeniser:
 
     def tokenise(self, text: str) -> list[str]:
         if type(text) == str:
-            words = text.split()
+            words = [word for word in text.split() if word]
             return words
         else:
             return 'Input text must be a string'
